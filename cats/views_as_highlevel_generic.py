2ec6b781f027d3b89bb52e163c73ed47ee731077
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-09-11 12:46:57 +0400

    Added tasks about views, generic, serializers.

---
+++ cats/views_as_highlevel_generic.py
@@ -0,0 +1,15 @@
+# Обновлённый views.py
+from rest_framework import generics
+
+from .models import Cat
+from .serializers import CatSerializer
+
+
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,43 @@
 from django.urls import path
 
-from cats.views import cat_list
+# from cats.views import cat_list
+
+# urlpatterns = [
+#    path('cats/', cat_list),
+# ]
+
+
+# from cats.views import APICat
+
+# urlpatterns = [
+#     path('cats/', APICat.as_view()),
+# ]
+from cats.views import CatList, CatDetail
 
 urlpatterns = [
-   path('cats/', cat_list),
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
 ]
 
+# urls.py for viewset
+# urls.py
+from rest_framework.routers import SimpleRouter
 
+from django.urls import include, path
+
+from cats.views import CatViewSet
+
+# Создаётся роутер
+router = SimpleRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
+
+urlpatterns = [
+    # Все зарегистрированные в router пути доступны в router.urls
+    # Включим их в головной urls.py
+    path('', include(router.urls)),
+] 
